878ceda9b00a02f6edd24a9ed6b5d75490c9fe00
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-06 14:27:45 +0530

    create server.js

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const connectDB = require("./config/db");
+const adminRoutes = require("./routes/adminRoutes");
+const userRoutes = require("./routes/userRoutes")
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+app.use(express.json());
+app.use("*", cors());
+
+app.get("/", (req, res) => {
+	res.send("API is Running");
+});
+
+app.use("/user/admin", adminRoutes);
+app.use("/user",userRoutes)
+
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server Started on port ${PORT}..`));
+
