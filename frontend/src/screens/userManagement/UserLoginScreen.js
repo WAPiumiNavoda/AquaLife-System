67fbee18b2a402504f037c23cb0a58ff40e17429
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-06 16:38:24 +0530

    Add styles for login page

--- frontend/src/screens/userManagement/UserLoginScreen.js
+++ frontend/src/screens/userManagement/UserLoginScreen.js
@@ -38,7 +38,7 @@ const Login = () => {
             <div className="row d-flex justify-content-center align-items-center h-100">
               <div className="col-md-9 col-lg-6 col-xl-5">
                 <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                  src="https://res.cloudinary.com/flocktogether/image/upload/v1680779080/6528419_i3qj6v.png"
                   className="img-fluid"
                   alt="Sample image"
                 />
@@ -105,26 +105,7 @@ const Login = () => {
               </div>
             </div>
           </div>
-          <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-            <div className="text-white mb-3 mb-md-0">
-              Copyright © 2020. All rights reserved.
-            </div>
-
-            <div>
-              <a href="/" className="text-white me-4">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="/" className="text-white me-4">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="/" className="text-white me-4">
-                <i className="fab fa-google"></i>
-              </a>
-              <a href="/" className="text-white">
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-            </div>
-          </div>
+         
         </section>
       </>
     );
