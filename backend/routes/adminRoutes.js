1d64e5a2c30e08f73742c1ae2e4acc2d7efd2b20
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-04-30 22:39:58 +0530

    create admin routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,17 @@
 const express = require("express");
-
+const { createProject, getProjectForEachAdmin,getProjectById} = require("../controllers/projectController");
+const { protect } = require("../middleware/authUserMiddleware");
 const router = express.Router();
+
+//Routes for Admin Project management Operations
+router.route("/projectCreate").post(protect,createProject);
+router.route("/getProjects/:id").get(protect, getProjectForEachAdmin);
+router
+	.route("/getProject/:id")
+	.get(protect, getProjectById)
+	// .put(protect, updateTrainerLeave)
+	// .delete(protect, deleteTrainerLeave);
+
 
 
 module.exports = router;
