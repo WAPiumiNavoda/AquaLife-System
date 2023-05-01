f677de8d8e328eda2e1725d96d29efd25bef0909
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-01 10:29:09 +0530

    protect the routes

--- backend/routes/sunmitInnovationRoutes.js
+++ backend/routes/sunmitInnovationRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { protect } = require('../middleware/authUserMiddleware');
 const {getInnovation,
        getInnovationById,
        createInnovation} = require('../controllers/submitInnovationController');
 
-router.route('/').get(getInnovation);
-router.route('/create').post(createInnovation);
-router.route('/:id').get(getInnovationById);
+router.route('/').get(protect,getInnovation);
+router.route('/create').post(protect,createInnovation);
+router.route('/:id').get(protect,getInnovationById);
 // .put(protect,UpdateCategory).delete(protect,DeleteCategory);
 
 module.exports = router;
