9fbf498e6af1bd24a86d811762d654d7c2dbce50
ACER <it20617400@my.sliit.lk>
2023-05-04 22:04:21 +0530

    homepage created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
 import React from "react";
 import UserRegisterScreen from "./screens/userManagement/UserRegisterScreen";
 import UserLoginScreen from "./screens/userManagement/UserLoginScreen";
@@ -7,26 +7,27 @@ import InnovationHome from "./screens/InnovationManagment/InnovationHome";
 import Innovation from "./screens/InnovationManagment/Innovation";
 import AllInnovation from "./screens/InnovationManagment/AllInnovation";
 import OneInnovation from "./screens/InnovationManagment/OneInnovation";
-
-
+import Home from "./components/Home/Home";
+import Header from "./components/Home/Header";
+import Footer from "./Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-       <main>
-      <Routes>
-      <Route path = "/user-register" element={<UserRegisterScreen/>}/>
-      <Route path = "/user-login" element={<UserLoginScreen/>} />
-      <Route path = "/innovation" element={<Innovation/>} />
-      <Route path = "/innovationAll" element={<AllInnovation/>} />
-      <Route path = "/oneInnovation" element={<OneInnovation/>} />
-      </Routes> 
-       </main>
-       
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user-register" element={<UserRegisterScreen />} />
+          <Route path="/user-login" element={<UserLoginScreen />} />
+          <Route path="/innovation" element={<Innovation />} />
+          <Route path="/innovationAll" element={<AllInnovation />} />
+          <Route path="/oneInnovation" element={<OneInnovation />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
-
-
