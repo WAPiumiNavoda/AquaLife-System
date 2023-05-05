1e2407e795a825af484ad26972d28fb24bf983c3
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-05-05 19:21:55 +0530

    create project view page

---
+++ frontend/src/screens/projectManagement/ProjectViewPage.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const ProjectView=()=>{
+    return(
+        <>
+        <h1 className="text-center text-success my-5">Projects</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+               
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-3">
+                    <div class="card">
+                        <img src="https://www.shutterstock.com/image-photo/water-scarcity-clean-projects-africa-260nw-246601225.jpg"class="card-img-top" alt="Card image cap"/>
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                  
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default ProjectView
