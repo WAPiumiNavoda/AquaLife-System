f9bc80c403a09ed375df54c2f903ef918267e81b
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-05 19:18:26 +0530

    create innovation support controller

---
+++ backend/controllers/supportInnovationController.js
@@ -0,0 +1,23 @@
+const SupportInnovations = require('../models/supportInnovationModel');
+const asyncHandler = require('express-async-handler');
+
+//Create innovation controller
+const  createSupportInnovation = asyncHandler(async (req, res) => {
+  const { suppotorName,suppotorEmail,supportorPhone, price } = req.body;
+
+  if (! suppotorName || !suppotorEmail || !supportorPhone|| !price ) {
+    res.status(400);
+    throw new Error("Please Fill all the feilds");
+  } else {
+    const SupportInnovation = new SupportInnovations({ suppotorName,suppotorEmail,suppotorEmail,supportorPhone });
+
+    const createdSupportInnovation = await SupportInnovation.save();
+
+    res.status(201).json(createdSupportInnovation);
+  }
+});
+
+//export all functions
+module.exports = {
+     createSupportInnovation
+}
