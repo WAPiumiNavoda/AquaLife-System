dc4815ae2ce4dfed805e2a6cb8c7a0100d1e2258
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-06 13:16:29 +0530

    innovation support delete controller

--- backend/controllers/supportInnovationController.js
+++ backend/controllers/supportInnovationController.js
@@ -43,9 +43,28 @@ try {
   }
 });
 
+//delete innovation support
+const deleteInnovationSupport = asyncHandler(async (req, res) => {
+  const innovationSup = await SupportInnovations.findById(req.params.id);
+
+  // if ( innovationSup.user.toString() !== req.user._id.toString()) {
+  //   res.status(401);
+  //   throw new Error("You can't perform this action");
+  // }
+
+  if ( innovationSup ) {
+    await  innovationSup.remove();
+    res.json({ message: "Innovation item is removed" });
+  } else {
+    res.status(404);
+    throw new Error("This kind of innovation is not Found");
+  }
+});
+
 //export all functions
 module.exports = {
      createSupportInnovation,
      getSupportInnovation,
-     innovationSupportApprove
+     innovationSupportApprove,
+     deleteInnovationSupport
 }
