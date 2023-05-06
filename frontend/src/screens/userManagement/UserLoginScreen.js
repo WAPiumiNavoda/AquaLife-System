3d4931a6da4aa2275a90f3ecd130ec73eec2843e
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-05-06 14:15:51 +0530

    link dashboard

--- frontend/src/screens/userManagement/UserLoginScreen.js
+++ frontend/src/screens/userManagement/UserLoginScreen.js
@@ -20,7 +20,7 @@ const Login = () => {
         .then((response) => {
           console.log(response);
           if (response.data.role === "Admin") {
-            navigate("/admin-home");
+            navigate("/dashboard");
           } else if (response.data.role === "User") {
             navigate("/user-home");
           }
