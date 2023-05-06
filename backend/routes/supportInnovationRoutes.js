b09dbb68c58cca81074501756a599da218726cf6
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-06 13:10:25 +0530

    approve support innovation route

--- backend/routes/supportInnovationRoutes.js
+++ backend/routes/supportInnovationRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const {
       createSupportInnovation,
-      getSupportInnovation
+      getSupportInnovation,
+      innovationSupportApprove
        } = require('../controllers/supportInnovationController');
 
 
 router.route('/create').post(createSupportInnovation);
 router.route('/').get(getSupportInnovation);
+router.route('/:id').post(innovationSupportApprove);
 
 module.exports = router;
