6ac4f162eb272cff4e93f9a4c43a0228749d1eee
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-06 13:45:31 +0530

    innovation deny action

--- frontend/src/actions/innovationActions.js
+++ frontend/src/actions/innovationActions.js
@@ -11,7 +11,10 @@ import {
     INNOVATION_CREATE_FAIL,
     INNOVATION_APPROVE_REQUEST,
     INNOVATION_APPROVE_SUCCESS,
-    INNOVATION_APPROVE_FAIL
+    INNOVATION_APPROVE_FAIL,
+    INNOVATION_DENY_REQUEST,
+    INNOVATION_DENY_SUCCESS,
+    INNOVATION_DENY_FAIL
 } from '../constants/innovationConstants'
 
 
@@ -168,3 +171,49 @@ export const approveInnovationAction = ( id ) => async (
   }
 };
 
+export const approveDenyAction = ( id ) => async (
+  dispatch,
+  getState
+) => {
+  try {
+    dispatch({
+      type: INNOVATION_DENY_REQUEST,
+    });
+
+    // const {
+    //   userLogin: { userInfo },
+    // } = getState();
+
+    // const config = {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${userInfo.token}`,
+    //   },
+    // };
+
+    const { data } = await axios.post(
+      `http://localhost:5000/innovation/deny/${id}`,
+    );
+     swal({
+			title: "Success !!!",
+			text: "Your innovation approved.",
+			icon: "success",
+			timer: 2000,
+			button: false,
+		});
+
+    dispatch({
+      type: INNOVATION_DENY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({
+      type: INNOVATION_DENY_FAIL,
+      payload: message,
+    });
+  }
+};
