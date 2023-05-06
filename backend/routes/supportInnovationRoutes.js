39515d00271558be542cf3f32185a1368aa336e4
Piumi Navoda <it20617028@my.sliit.lk>
2023-05-06 13:16:46 +0530

    innovation support delete route

--- backend/routes/supportInnovationRoutes.js
+++ backend/routes/supportInnovationRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
       createSupportInnovation,
       getSupportInnovation,
-      innovationSupportApprove
+      innovationSupportApprove,
+      deleteInnovationSupport
        } = require('../controllers/supportInnovationController');
 
 
 router.route('/create').post(createSupportInnovation);
 router.route('/').get(getSupportInnovation);
 router.route('/:id').post(innovationSupportApprove);
+router.route('/delete/:id').delete(deleteInnovationSupport);
 
 module.exports = router;
