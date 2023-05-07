321e3bfdfa604ec1a444d6998357d75888ca2164
ACER <it20617400@my.sliit.lk>
2023-05-07 13:02:31 +0530

    quality test page created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,52 +3,59 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import {
-	userLoginReducer,
-	userRegisterReducer,
-	//userViewReducer,
-	//userUpdateReducer,
+  userLoginReducer,
+  userRegisterReducer,
+  //userViewReducer,
+  //userUpdateReducer,
 } from "./reducers/userReducers";
-import {InnovationCreateReducer,
-	 innovationListReducer,
-	 innovationOneReducer,
-	 InnovationAcceptReducer, 
-	  InnovationApproveListReducer,
-	 InnovationDenyReducer} from './reducers/innovationReducers';
-import {articleListReducer}  from './reducers/articleReducers';
+import {
+  InnovationCreateReducer,
+  innovationListReducer,
+  innovationOneReducer,
+  InnovationAcceptReducer,
+  InnovationApproveListReducer,
+  InnovationDenyReducer,
+} from "./reducers/innovationReducers";
+import { articleListReducer } from "./reducers/articleReducers";
 
-import { InnovationSupportCreateReducer,
-	     InnovationSupportListReducer,
-		
-		 } from "./reducers/innovationSupportReducers";
+import {
+  InnovationSupportCreateReducer,
+  InnovationSupportListReducer,
+} from "./reducers/innovationSupportReducers";
 import { ProjectCreateReducer } from "./reducers/projectReducers";
-
+import { qualityListReducer } from "./reducers/qualityTestReducers";
 
 const reducer = combineReducers({
-	user_Login: userLoginReducer,
-	userRegistration: userRegisterReducer,
-	innovationList: innovationListReducer,
-	innovationOne: innovationOneReducer,
-	articleList: articleListReducer,
-	innovationCreate: InnovationCreateReducer,
-	innovationSupportCreate: InnovationSupportCreateReducer,
-	innovationSupportList: InnovationSupportListReducer,
-	projectCreate:ProjectCreateReducer,
-	innovationApprove:InnovationAcceptReducer,
-	innovationDeny: InnovationDenyReducer,
-    innovationSet : InnovationApproveListReducer
+  user_Login: userLoginReducer,
+  userRegistration: userRegisterReducer,
+  innovationList: innovationListReducer,
+  innovationOne: innovationOneReducer,
+  articleList: articleListReducer,
+  innovationCreate: InnovationCreateReducer,
+  innovationSupportCreate: InnovationSupportCreateReducer,
+  innovationSupportList: InnovationSupportListReducer,
+  projectCreate: ProjectCreateReducer,
+  innovationApprove: InnovationAcceptReducer,
+  innovationDeny: InnovationDenyReducer,
+  innovationSet: InnovationApproveListReducer,
+  qualityList: qualityListReducer,
 });
 
-const userInfoFromStorage = localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null;
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
 //console.log(userInfoFromStorage);
 
-
 const initialState = {
-	user_Login: { userInfo: userInfoFromStorage },
-	
+  user_Login: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;
