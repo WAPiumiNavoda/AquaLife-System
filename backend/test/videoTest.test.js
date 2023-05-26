622447d9595e1705a578846ba85b82615322b1cf
ACER <it20617400@my.sliit.lk>
2023-05-26 23:52:47 +0530

    quality test cases added

---
+++ backend/test/videoTest.test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const { createVideo } = require("../controllers/videoController");
+
+describe("Testing the project routes", () => {
+	it("Create Video Test", async () => {
+		const returnedProject = createVideo({
+			videoCategory: "test category",
+			videoFile: "https://icon-library.com/images/videographer-icon/videographer-icon-24.jpg",
+			videoTitle: "test title",
+			videoCaption: "test caption",
+			dateofUpload: "test date",
+		});
+		const project = {
+			Name: "VIDEOTEST",
+		};
+		expect(returnedProject.name).to.equal(project.name);
+	});
+});
