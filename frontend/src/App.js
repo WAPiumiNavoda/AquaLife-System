429d09ee70c92fdce0f31086fbd8e6a2769cf1d1
Rashmi Bhagya <89590611+RashmiBhagya@users.noreply.github.com>
2023-05-27 12:55:35 +0530

    new one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,6 +92,7 @@ const App = () => {
           <Route path="/oneArticle" element={<OneArticle />} />
           <Route path="/articleReport" element={<ArticalList />} />
           <Route path="/articleReportList" element={<ArticalListReport />} />
+          
           {/* project managment */}
           <Route path="/user-projectView" element={<ProjectView />} />
           <Route path="/admin-createProject" element={<ProjectCreate />} />
@@ -100,7 +101,6 @@ const App = () => {
           <Route path="/projectList" element={<ProjectListByAdmin />} />
 
           {/* Donate management */}
-
           <Route path = "/createLabour" element={<LabourCreate/>}/>
           <Route path = "/laborList" element={<LaborList/>}/>
           <Route path = "/laborListForAdmin"  element={< LaborListForAdmin/>}/>
